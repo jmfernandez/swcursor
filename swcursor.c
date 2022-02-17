c14f6660e99c6f512439fd6caf3967448f582933
BillGR17 <11507104+BillGR17@users.noreply.github.com>
2022-02-17 23:49:30 +0200

    Restored original code allow only 1 cursor

--- swcursor.c
+++ swcursor.c
@@ -4,63 +4,15 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <X11/Xlib.h>
-#define SECOND 1000000
-typedef struct {
-  cairo_surface_t* image;
-  guint64 timestamp;
-  guint64 framerate;
-} State_t;
 static cairo_surface_t* load_image(const char* path);
-static void show_main_window(State_t* state);
+static void show_main_window(cairo_surface_t* image;);
 static gboolean tick(GtkWidget* widget, GdkFrameClock* frame_clock, gpointer user_data);
-static State_t* state;
-static void cleanup() {
-  free(state);
-  return;
-}
 int main(int argc, char** argv) {
-  int opt;
-  cairo_surface_t* image;
-  signal(SIGABRT, cleanup);
-  state = (State_t*)malloc(sizeof(State_t));
-  state->framerate = SECOND / 60;
-  state->timestamp = 0;
-  while ((opt = getopt(argc, argv, "irh")) != -1) {
-    switch (opt) {
-      case 'i':
-        if (optind < argc) {
-          char* file = argv[optind];
-          image = load_image(file);
-        }
-        break;
-      case 'r': {
-        int rate = atoi(argv[optind]);
-        if (rate == 0 || rate > SECOND) {
-          fprintf(stdout, "Could not parse: %s, setting default rate to 60", argv[optind]);
-        } else {
-          state->framerate = SECOND / rate;
-        }
-        break;
-      }
-      case 'h':
-        fprintf(stdout,
-                "Usage: %s [-ir] [file...]\n\n-i [file.png]\n load the png file as mouse cursor\n\n-r [number]\nset "
-                "the refresh rate per seconds\n\nExample:\n./swcursor -i cursors/cursor-large.png -r 120",
-                argv[0]);
-        exit(EXIT_SUCCESS);
-      default: fprintf(stderr, "Type -h to see help\n", argv[0]); exit(EXIT_FAILURE);
-    }
-  }
-  if (image != NULL) {
-    state->image = image;
-  } else {
-    state->image = load_image("cursors/8.png");
-  }
-  fprintf(stdout, "Refreshing cursor with %d fps", state->framerate);
   gtk_init(&argc, &argv);
-  show_main_window(state);
+  cairo_surface_t* image;
+  image = load_image("cursor.png");
+  show_main_window(image);
   gtk_main();
   return 0;
 }
@@ -68,16 +20,16 @@ static cairo_surface_t* load_image(const char* path) {
   cairo_surface_t* image = cairo_image_surface_create_from_png(path);
   if (cairo_surface_status(image) != CAIRO_STATUS_SUCCESS) {
     const char* msg = cairo_status_to_string(cairo_surface_status(image));
-    fprintf(stderr, "Error loading '%s': %s\n", path, msg);
-    return NULL;
+    fprintf(stderr, "swcursor: error loading '%s': %s\n", path, msg);
+    exit(1);
   }
   return image;
 }
-static void show_main_window(State_t* state) {
+static void show_main_window(cairo_surface_t* image) {
   SWCursorWindow* window;
   window = swcursor_window_new();
-  swcursor_window_set_image(window, state->image);
-  gtk_widget_add_tick_callback(GTK_WIDGET(window), tick, state, free);
+  swcursor_window_set_image(window, image);
+  gtk_widget_add_tick_callback(GTK_WIDGET(window), tick, NULL, NULL);
   gtk_widget_show_all(GTK_WIDGET(window));
 }
 static gboolean tick(GtkWidget* widget, GdkFrameClock* frame_clock, gpointer user_data) {
@@ -92,29 +44,22 @@ static gboolean tick(GtkWidget* widget, GdkFrameClock* frame_clock, gpointer use
   int move_x, move_y;
   unsigned int mask;
   gint scale_factor;
-  // gboolean mouse_down;
-  int w_width, w_height;
-  State_t* state = (State_t*)user_data;
-  guint64 timestamp = gdk_frame_clock_get_frame_time(frame_clock);
-  if ((timestamp - state->timestamp) >= state->framerate) {
-    state->timestamp = gdk_frame_clock_get_frame_time(frame_clock);
-    gtk_window_get_size(GTK_WINDOW(widget), &w_width, &w_height);
-    gdk_window = gtk_widget_get_window(widget);
-    xdisplay = GDK_SCREEN_XDISPLAY(gdk_window_get_screen(gdk_window));
-    xroot_window = XDefaultRootWindow(xdisplay);
-    scale_factor = gdk_window_get_scale_factor(gdk_window);
-    if (XQueryPointer(xdisplay, xroot_window, &ret_root, &ret_child, &root_x, &root_y, &win_x, &win_y, &mask)) {
-      move_x = root_x / scale_factor - w_height / 2;
-      move_y = root_y / scale_factor - w_width / 2;
-      gtk_window_move(GTK_WINDOW(widget), move_x, move_y);
-      // mouse_down = (mask & Button1Mask) || (mask & Button2Mask) || (mask & Button3Mask);
-      // swcursor_window_set_mouse_down(SWCURSOR_WINDOW(widget), mouse_down);
-    } else {
-      if (show_warning) {
-        fprintf(stderr,
-                "swcursor: warning: could not get cursor position from Xserver (further warnings will be suppressed)");
-        show_warning = FALSE;
-      }
+  gboolean mouse_down;
+  gdk_window = gtk_widget_get_window(widget);
+  xdisplay = GDK_SCREEN_XDISPLAY(gdk_window_get_screen(gdk_window));
+  xroot_window = XDefaultRootWindow(xdisplay);
+  scale_factor = gdk_window_get_scale_factor(gdk_window);
+  if (XQueryPointer(xdisplay, xroot_window, &ret_root, &ret_child, &root_x, &root_y, &win_x, &win_y, &mask)) {
+    move_x = root_x / scale_factor - 63;
+    move_y = root_y / scale_factor - 63;
+    gtk_window_move(GTK_WINDOW(widget), move_x, move_y);
+    mouse_down = (mask & Button1Mask) || (mask & Button2Mask) || (mask & Button3Mask);
+    swcursor_window_set_mouse_down(SWCURSOR_WINDOW(widget), mouse_down);
+  } else {
+    if (show_warning) {
+      fprintf(stderr,
+              "swcursor: warning: could not get cursor position from Xserver (further warnings will be suppressed)");
+      show_warning = FALSE;
     }
   }
   return G_SOURCE_CONTINUE;
